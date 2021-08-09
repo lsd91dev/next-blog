2526356f891647245cfef9a5bf9cda9857deedee
lsd91dev <lsd91dev@gmail.com>
2021-08-09 09:54:24 +0200

    Refactor
    Refactored: PostModel to PostJSON and now returns Readonly object. Created method extractId to get id from file name

--- src/core/file-reader/file-reader.ts
+++ src/core/file-reader/file-reader.ts
@@ -1,10 +1,10 @@
 import path from "path";
 import fs from "fs";
 import { Post } from "../../features/post/domain/post";
-import { PostModel } from "../../features/post/model/post";
 import { singleton } from "tsyringe";
 import { Markdown } from "./markdown";
-import {extractId} from "../id/extract-id";
+import { extractId } from "../id/extract-id";
+import {PostJSON} from "../../features/post/domain/post-json";
 
 @singleton()
 export class FileReader {
@@ -18,10 +18,10 @@ export class FileReader {
         this.FILES_ROOT_DIRECTORY = fs.readdirSync(this.ROOT_DIRECTORY);
         return this.FILES_ROOT_DIRECTORY.map( (fileName) => {
             // TODO better refactor
-            const id = extractId(fileName);
-            const content = this.readFile(fileName)
-            const parsed = this.file.render(content);
-            return PostModel.create(id,id,parsed)
+            const id: string = extractId(fileName);
+            const content: string = this.readFile(fileName)
+            const parsed: string = this.file.render(content);
+            return PostJSON.create(id, id, parsed);
         })
     }
 
@@ -33,6 +33,5 @@ export class FileReader {
         const fullPath = path.join(pathFile, fileName);
         return fs.readFileSync(fullPath, 'utf-8');
     }
-
 
 }
---
+++ src/features/post/domain/post-json.ts
@@ -0,0 +1,12 @@
+import {Post} from "./post";
+
+export class PostJSON {
+    static create(id: string, title: string, content: string){
+        const post: Readonly<Post> = {
+            id,
+            title,
+            content
+        }
+        return post
+    }
+}
